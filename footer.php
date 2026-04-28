56b29521ba7fff4a0c2e4f705e60e35e0abdece5
Isabel Morro Tabares <isabelmorrotabares@MacBook-Air-de-Isabel.local>
2026-04-28 09:48:41 +0200

    primer commit basico

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer> 
+    <p> <a href="contacta.php"> Contáctanos </a> </p>
+    <!-- Meter como_se_hizo !!!!!!! -->
+    <p> &copy; 2026. Todos los derechos reservados. </p>
+</footer>
---
+++ header.php
@@ -0,0 +1,31 @@
+<header>
+    
+    <h1 class="text-center"> Gestión de reservas de actividades </h1>
+    
+    <!-- Copiado del ej de clase del otro dia, puede servir -->
+    <nav class="navbar navbar-expand-sm bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"> Menú </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <!-- Cambiar href -->
+                        <a class="nav-link active" aria-current="page" href="./index.php"> Inicio </a>
+                    </li>
+                    <li class="nav-item">
+                        <!-- Cambiar href -->
+                        <a class="nav-link" href="./actividades.php"> Actividades </a>
+                    </li>
+                    <li class="nav-item">
+                        <!-- Cambiar href -->
+                        <a class="nav-link" href="./reservas.php"> Reservas </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</header>
